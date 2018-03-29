4a991c3ff65dc3e795c3d63e489565f3adbd3064
Dmitry Ivanov <it-develops@yandex.ru>
2018-03-29 11:51:58 +0300

    edit

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
@@ -17,6 +12,11 @@ namespace PartyInvites.Controllers
             ViewBag.Greeting = hour < 12 ? "Доброе утро!" : "Добрый вечер!";
 
             return View("MyView");
+        }
+
+        public ViewResult RsvpForm()
+        {
+            return View();
         }
     }
 }
