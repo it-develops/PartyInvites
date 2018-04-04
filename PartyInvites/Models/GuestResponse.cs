86e7e8f6ad0e0d5094e6cd86f74126feeeb6fa87
Dmitry Ivanov <it-develops@yandex.ru>
2018-04-04 12:44:24 +0300

    edit

--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -1,15 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace PartyInvites.Models
 {
     public class GuestResponse
     {
-        public string Name { get; set; }
+        [Required(ErrorMessage ="Пожалуйста введите Ваше имя")]
+        public string Name { get; set;}
+
+        [Required(ErrorMessage = "Пожалуйста введите Ваш Email адрес")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage ="Пожалуйста введите корректный адрес Email")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста введите Ваш номер телефона")]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста укажите, примите ли Вы участие")]
         public bool? WillAttend { get; set; }
     }
 }
